f87afc933b81b17d9bfa7de45c4f2268785b6a68
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-11-16 12:15:38 +0300

    feat: add base pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,14 @@
 <script lang="ts" setup></script>
 
 <template>
-    <div class="__home"></div>
+    <div class="__home">
+        <div class="p-4">
+            <UAlert
+                title="Home page"
+                description="Maybe a landing in the future"
+            ></UAlert>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped></style>
