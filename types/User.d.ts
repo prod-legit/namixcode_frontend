83591b8c55dd23efae900b75e22bf66ed5dfdc30
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-11-16 18:52:50 +0300

    feat: expand User type with Organization and Person interfaces

--- types/User.d.ts
+++ types/User.d.ts
@@ -1,9 +1,24 @@
 interface Organization {
-
+    id: string;
+    email: string;
+    name: string;
+    description: string;
+    location: string;
+    logo: string;
+    foundation_year: number;
+    scope: string;
 }
 
 interface Person {
-	
+    id: string;
+    name: string;
+    phone: string;
+    sex: "male" | "female";
+    birthdate: string;
+    email: string;
+    experience: number;
+    professions: string[];
+    interests: string[];
 }
 
-export type User = {};
+export type User = Organization | Person;
