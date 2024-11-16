c15f5fc7bb6c93345d5ab3d3df22028bd7c0950a
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-11-16 10:20:54 +0300

    Initial commit 🤡

--- app/app.config.ts
+++
@@ -1,7 +0,0 @@
-export default defineAppConfig({
-    seo: {
-        title: "Andrei Hudalla",
-        description: "software is fun",
-        image: "https://andreihudalla.ru/portrait.png",
-    },
-});
