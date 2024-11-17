8ae6e7a7bf7209e3be9f998118fdb1790e760f0d
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-11-17 06:14:42 +0300

    chore: remove auth-utils, add seo and markdown renderer modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,34 @@
 export default defineNuxtConfig({
     compatibilityDate: "2024-04-03",
     devtools: { enabled: true },
-    modules: ["@nuxt/ui", "nuxt-auth-utils", "@nuxt/fonts"],
+    modules: ["@nuxt/ui", "@nuxt/fonts", "@nuxtjs/seo", "@nuxtjs/mdc"],
     colorMode: {
         preference: "light",
         fallback: "light",
     },
     css: ["~/assets/scss/global.scss"],
+    site: {
+        name: "Starlink",
+        description: "Спросите звёзды™",
+    },
+    app: {
+        pageTransition: {
+            name: "page",
+            mode: "in-out",
+        },
+        layoutTransition: {
+            name: "page",
+            mode: "in-out",
+        },
+    },
+    mdc: {
+        headings: {
+            anchorLinks: false,
+        },
+    },
+    routeRules: {
+        "/": {
+            redirect: "/register",
+        },
+    },
 });
