f19769f64509da1e926e0c46da4190e49021ab9f
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-11-17 06:04:39 +0300

    feat: add markdown renderer component

---
+++ components/common/mdcontain.vue
@@ -0,0 +1,83 @@
+<script lang="ts" setup>
+defineProps<{
+    value: string;
+}>();
+</script>
+
+<template>
+    <div class="md-formatter">
+        <MDC :value="value" />
+    </div>
+</template>
+
+<style lang="scss">
+.md-formatter {
+    > div {
+        > * {
+            @apply mb-4;
+        }
+        h1 {
+            @apply text-2xl font-bold opacity-100 sm:text-3xl;
+        }
+        h2 {
+            @apply text-xl font-semibold opacity-100 sm:text-2xl;
+        }
+        h3 {
+            @apply text-lg font-semibold opacity-90 sm:text-xl;
+        }
+        h4 {
+            @apply text-base font-normal opacity-90 sm:text-lg;
+        }
+        h5 {
+            @apply text-sm font-normal opacity-80 sm:text-base;
+        }
+        h6 {
+            @apply text-sm font-normal opacity-80;
+        }
+        p {
+            @apply opacity-80;
+        }
+        ul {
+            li {
+                @apply ml-6 list-disc;
+            }
+        }
+        ol {
+            li {
+                @apply ml-6 list-decimal;
+            }
+        }
+        img {
+            @apply rounded-xl;
+        }
+        video {
+            @apply rounded-xl;
+        }
+        blockquote {
+            @apply rounded-l-md border-l-4 border-gray-400 pl-4 opacity-70;
+        }
+        table {
+            @apply overflow-hidden rounded-xl outline outline-1 outline-gray-400 dark:outline-gray-600;
+            thead {
+                @apply text-left;
+                th {
+                    @apply rounded-xl border border-gray-400 border-opacity-50 px-4 py-2;
+                }
+            }
+            tbody {
+                td {
+                    @apply border border-gray-400 border-opacity-40 px-4 py-2;
+                }
+            }
+        }
+        a {
+            &:hover {
+                @apply text-primary dark:text-primary;
+            }
+        }
+        hr {
+            @apply dark:opacity-20;
+        }
+    }
+}
+</style>
